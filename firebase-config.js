9d2c1c32fcbce0bf461ed131a543e4557ad90be3
SeungHeon Baek <158144807+100-heon@users.noreply.github.com>
2025-11-13 16:22:22 +0900

    으으

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+﻿window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
+  apiKey: "AIzaSyA-1Sy0s28yyDdH5ki_zbiIJ5GH3Hc8m4o",
+  authDomain: "endline-photo.firebaseapp.com",
+  projectId: "endline-photo",
+  storageBucket: "endline-photo.firebasestorage.app",
+  messagingSenderId: "219246935723",
+  appId: "1:219246935723:web:95d305f0a78831f92301ee"
+};
